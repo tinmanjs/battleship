47ac7bbb6eecd9c926a9021933bc459e0b4c9397
tinmanjs <tinmanjs@protonmail.com>
2020-09-19 20:45:41 -0400

    Created the user and computer boards

--- app.js
+++ app.js
@@ -1,24 +1,36 @@
 console.log("BATTLESHIP!")
 
-const getAllElements = () => {
-    const userGrid = document.querySelector('.grid-user')
-    const computerGrid = document.querySelector('.grid-computer')
-    const displayGrid = document.querySelector('.grid-display')
-    const ships = document.querySelectorAll('.ship')
-
-    const destoyer = document.querySelector('destroyer-container')
-    const submarine = document.querySelector('submarine-container')
-    const cruiser = document.querySelector('cruiser-container')
-    const battleship = document.querySelector('battleship-container')
-    const carrier = document.querySelector('carrier-container')
-
-    const startButton = document.querySelector('#start')
-    const rotateButton = document.querySelector('#rotate')
-    const turnDisplay = document.querySelector('#who-goes')
-    const infoDisplay = document.querySelector('#info')
-}
-
 document.addEventListener('DOMContentLoaded', () => {
-    getAllElements()
+  const userGrid = document.querySelector('.grid-user')
+  const computerGrid = document.querySelector('.grid-computer')
+  const displayGrid = document.querySelector('.grid-display')
+  const ships = document.querySelectorAll('.ship')
+
+  const destoyer = document.querySelector('destroyer-container')
+  const submarine = document.querySelector('submarine-container')
+  const cruiser = document.querySelector('cruiser-container')
+  const battleship = document.querySelector('battleship-container')
+  const carrier = document.querySelector('carrier-container')
+
+  const startButton = document.querySelector('#start')
+  const rotateButton = document.querySelector('#rotate')
+  const turnDisplay = document.querySelector('#who-goes')
+  const infoDisplay = document.querySelector('#info')
+
+  const width = 10
+  const userSquares = []
+  const computerSquares = []
     
+  const createBoard = (grid, squares) => {
+    for (let i = 0; i < width*width; i++) {
+      const square = document.createElement('div')
+      square.dataset.id = i
+      grid.appendChild(square)
+      squares.push(square)
+    }
+  }
+
+  createBoard(userGrid, userSquares)
+  createBoard(computerGrid, userSquares)
+  
 })
